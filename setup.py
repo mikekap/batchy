83a187f4df2411870f9c8fa4b72330fe67ef62fe
Mike Kaplinskiy <mikekap@vineapp.com>
2014-04-26 13:11:11 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+import itertools
+import versioneer
+from setuptools import setup, find_packages
+from distutils.command.sdist import sdist
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'batchy/_version.py'
+versioneer.versionfile_build = 'batchy/_version.py'
+versioneer.tag_prefix = ''
+versioneer.parentdir_prefix = 'batchy-'
+
+setup(
+    name='batchy',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='vine batching framework',
+    author='vine',
+    author_email='mikekap@vine.co',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ]
+)
+
