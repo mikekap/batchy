c5fa5866286863670ad2158e56b4a62c789bb141
Mike Kaplinskiy <mkaplinskiy@twitter.com>
2014-05-29 19:15:29 -0400

    Expose RunLoopLocal from the top-level package.

--- batchy/__init__.py
+++ batchy/__init__.py
@@ -2,5 +2,6 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from .runloop import runloop_coroutine, coro_return, deferred, future, current_run_loop
 from .batch_coroutine import batch_coroutine, class_batch_coroutine
+from .local import RunLoopLocal
+from .runloop import runloop_coroutine, coro_return, deferred, future, current_run_loop
